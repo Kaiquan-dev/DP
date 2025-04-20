093323f441a68ef27fc64e855a030a134698e72d
Kaiquan-dev <football133@mail.ru>
2025-04-20 22:49:11 +0300

    first

--- my_project/my_app/admin.py
+++ my_project/my_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from my_app.models import User
+from my_app.models import Book
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Book)
--- my_project/my_app/models.py
+++ my_project/my_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.core.validators import EmailValidator
 
-# Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=100, blank=False)
+    user_surname = models.CharField(max_length=100, blank=False)
+    user_date_of_birth = models.DateField(null=True, blank=True)
+    user_email = models.EmailField(
+        max_length=255,
+        unique=True,
+        validators=[EmailValidator()],
+    )
+    user_password = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f'{self.user_surname} {self.user_name}'
+    
+
+class Book(models.Model):
+    book_title = models.CharField(max_length=100, blank=False)
+    book_pages = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.book_title
